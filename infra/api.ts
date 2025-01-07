9ef62fedaa40c0a974d185c4fff467042f8f485a
Jeremy Lee <jeremy@keenpayroll.ca>
2025-01-07 11:32:44 -0330

    testing if I can change the handler

--- infra/api.ts
+++ infra/api.ts
@@ -1,4 +1,6 @@
 // Create the API
 export const api = new sst.aws.ApiGatewayV2("Api", {});
 
-api.route("GET /notes", "packages/functions/src/create.handler");
+api.route("GET /notes", {
+  handler: "packages/functions/src/create.handler"
+});
