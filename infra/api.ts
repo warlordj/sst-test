bf2c2f588be8aacd36b8af4ea338e3dc1443903b
Jeremy Lee <jeremy@keenpayroll.ca>
2025-01-07 11:37:40 -0330

    testing if I can change the runtime

--- infra/api.ts
+++ infra/api.ts
@@ -2,6 +2,8 @@
 export const api = new sst.aws.ApiGatewayV2("Api", {});
 
 api.route("GET /notes", {
-  handler: "packages/functions/src/create.handler",
-  runtime: "nodejs20.x"
+  function: {
+    handler: "packages/functions/src/create.handler",
+  }
+  
 });
