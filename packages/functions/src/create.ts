59cbf8c90a07963ba4437f9883ca70f3b076f52f
Jeremy Lee <jeremy@keenpayroll.ca>
2025-01-07 11:54:48 -0330

    added intuit packages

--- packages/functions/src/create.ts
+++ packages/functions/src/create.ts
@@ -2,6 +2,13 @@
 import { Handler } from "aws-lambda";
 import OAuthClient from 'intuit-oauth';
 
+const oauthClient = new OAuthClient({
+    clientId: 'ABIHnYwYOVIQregKQpKdRZAyd94LCswuJj4Vaz6GGmwbaS1CH3',
+    clientSecret: 'rTay7f1t87hn9Jcm91XocxDqzJPu6T73qiUV0jmC',
+    environment: 'sandbox' || 'production',
+    redirectUri: 'https://d1k7d2979d9v40.cloudfront.net/auth',
+});
+
 export const handler: Handler = async (event) => {
 
     return "012345"
