1137a8782ce3c857fffc54046b021973dc66c413
Jeremy Lee <jeremy@keenpayroll.ca>
2025-01-07 10:28:37 -0330

    testing?

--- packages/functions/src/api.ts
+++ packages/functions/src/api.ts
@@ -2,7 +2,7 @@ import { Resource } from "sst";
 import { Handler } from "aws-lambda";
 import { Example } from "@notes/core/example";
 
-export const handler: Handler = async (event) => {
+export const handler: Handler = async (_event) => {
   return {
     statusCode: 200,
     body: `${Example.hello()} Linked to ${Resource.MyBucket.name}.`,
