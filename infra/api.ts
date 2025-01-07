c5751e35a5d67b4009b161bb0c1f2c6972ee6c68
Jeremy Lee <jeremy@keenpayroll.ca>
2025-01-07 11:15:09 -0330

    testing a simple lambda

--- infra/api.ts
+++ infra/api.ts
@@ -7,11 +7,4 @@ export const api = new sst.aws.ApiGatewayV2("Api", {
   },
 });
 
-api.addRoutes(this, {
-  "GET /notes": {
-    function: {
-      handler: "packages/functions/src/create.handler",
-      runtime: "nodejs22.x", // Ensure Node.js 22.x runtime for this specific function
-    },
-  },
-});
+api.route("GET /notes", "packages/functions/src/create.handler");
