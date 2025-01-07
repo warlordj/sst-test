904f88803df4db4b3248a1ec4e12dbcb6622439f
Jeremy Lee <jeremy@keenpayroll.ca>
2025-01-07 11:47:08 -0330

    testing intuit api

--- packages/functions/src/create.ts
+++ packages/functions/src/create.ts
@@ -1,18 +1,21 @@
-//import * as uuid from "uuid";
-//import { Resource } from "sst";
-//import { APIGatewayProxyEvent, Handler } from "aws-lambda";
-//import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-//import { PutCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
-
-//const dynamoDb = DynamoDBDocumentClient.from(new DynamoDBClient({}));
 
 import { Handler } from "aws-lambda";
+import OAuthClient from 'intuit-oauth';
+
+const oauthClient = new OAuthClient({
+    clientId: 'ABIHnYwYOVIQregKQpKdRZAyd94LCswuJj4Vaz6GGmwbaS1CH3',
+    clientSecret: 'rTay7f1t87hn9Jcm91XocxDqzJPu6T73qiUV0jmC',
+    environment: 'sandbox' || 'production',
+    redirectUri: 'https://d1k7d2979d9v40.cloudfront.net/auth',
+});
+
+const authUri = oauthClient.authorizeUri({
+    scope: [OAuthClient.scopes.Accounting],
+    state: 'testState', // Optional state parameter for CSRF protection
+});
 
 export const handler: Handler = async (event) => {
 
-    console.log("made it here")
+    return "01234"
 
-    return "0123"
-
-    //return "mno"
 }
